cf2a6ba0b33ac901ff8e1e82bc4868eea396b14f
rems117 <rems117@yandex.ru>
2019-10-03 11:12:07 +0300

    adding class & script-example

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# class_TimedRotatingFileHandler
+TimedRotatingFileHandler с возможностью изменения имени бэкапов и с возможностью указания папки для бэкапов
+
+взят исходный код TimedRotatingFileHandler с https://github.com/python/cpython/blob/3.7/Lib/logging/handlers.py
+оригиналы измененных функций сохранены рядом
+
+при желании изменить суффикс, в __init__ нужно править для конкретного метода следующие строки
+self.suffix = "%Y-%m-%d"
+self.extMatch = r"^\d{4}-\d{2}-\d{2}(\.\w+)?$"
---
+++ example.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import os, sys, time
+import logging
+sys.dont_write_bytecode = True
+from class_TimedRotatingFileHandler import TimedRotatingFileHandler
+
+LOG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'LOGS_example')
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
+LOG_FILE = os.path.join(LOG_PATH, 'example.log')
+logger = logging.getLogger("TEST")
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s %(levelname)-5s %(name)-9s >> %(message)s", "%Y-%m-%d %H:%M:%S")
+
+sh = logging.StreamHandler()
+fh = TimedRotatingFileHandler(LOG_FILE, when='s', interval=3, backupCount=4)
+
+sh.setLevel(logging.DEBUG)
+fh.setLevel(logging.INFO)    
+sh.setFormatter(formatter)
+fh.setFormatter(formatter)
+logger.addHandler(sh)
+logger.addHandler(fh)    
+
+if __name__ == "__main__":    
+    while True:
+        try:
+            logger.info('INFO  message ')
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+            
